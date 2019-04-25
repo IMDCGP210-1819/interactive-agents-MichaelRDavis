4d71ff96d272d48d6747ace4f53edfaa5c9b3b5a
Michael Davis <MichaelRDavis1991@protonmail.com>
2019-04-25 20:05:00 +0100

    Added a 2D math vector.

--- MiniEngine/Source/Core/Math/Vector.h
+++ MiniEngine/Source/Core/Math/Vector.h
@@ -1,6 +1,28 @@
 #pragma once
 
-struct Vec2 
+struct Vec2
 {
+	Vec2();
+	Vec2(float inX, float inY);
+	Vec2(const Vec2& vec);
 
+	friend Vec2 operator+(const Vec2& vecA, const Vec2& vecB);
+	friend Vec2 operator-(const Vec2& vecA, const Vec2& vecB);
+	friend Vec2 operator*(const Vec2& vecA, const Vec2& vecB);
+	friend Vec2 operator/(const Vec2& vecA, const Vec2& vecB);
+
+	friend Vec2 operator*(const Vec2& vec, float scalar);
+	friend Vec2 operator*(float scalar, const Vec2& vec);
+
+	Vec2 operator+=(const Vec2& vec);
+	Vec2 operator-=(const Vec2& vec);
+	Vec2 operator*=(const Vec2& vec);
+	Vec2 operator/=(const Vec2& vec);
+
+	Vec2 operator*=(float scalar);
+
+	float x;
+	float y;
+
+	static const Vec2 zeroVector;
 };
