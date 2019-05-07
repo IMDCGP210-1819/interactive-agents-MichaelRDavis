7c90f9618318d820e1dd7606e8df0bc004eded41
Michael Davis <MichaelRDavis1991@protonmail.com>
2019-05-07 21:41:17 +0100

    Added comments to world class.

--- SpaceshipWars/Source/GameObject/World.h
+++ SpaceshipWars/Source/GameObject/World.h
@@ -6,22 +6,25 @@ class NavGrid;
 class Spaceship;
 class Asteroid;
 
+/**
+ * World object is container class for all the game objects in the world.
+ */
 class World
 {
 public:
-	/**  */
+	/** Constructor that initializes a renderer object. */
 	World(SDL_Renderer* renderer);
 
-	/**  */
+	/** Default destructor. */
 	~World();
 
-	/**  */
+	/** Initialize the world object. */
 	void Initialize();
 
-	/**  */
+	/** Update the world object, called once per frame. */
 	void Update(float deltaTime);
 
-	/**  */
+	/** Draw all objects in the game world. */
 	void Draw();
 
 #ifdef _DEBUG
@@ -36,22 +39,22 @@ public:
 #endif
 
 private:
-	/**  */
+	/** Cached pointer to the renderer object. */
 	SDL_Renderer* m_renderer;
 
-	/**  */
+	/** The background game object. */
 	std::unique_ptr<Background> m_background;
 
-	/**  */
+	/** The first spaceship. */
 	std::unique_ptr<Spaceship> m_mantis;
 
-	/**  */
+	/** The second spaceship. */
 	std::unique_ptr<Spaceship> m_scarab;
 
-	/**  */
+	/** The list of asteroids in the game world. */
 	std::unique_ptr<Asteroid> m_asteroid;
 
-	/**  */
+	/** A list of all the entities in the game world. */
 	std::vector<Entity*> m_entityList;
 
 	/** Current number of asteroids in the game world. */
